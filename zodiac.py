0d70b77936a505b2f7c9a52d525fd5e47f020f6f
dots <181677440+dots05@users.noreply.github.com>
2024-11-29 16:11:40 -0500

    Update zodiac.py

--- zodiac.py
+++ zodiac.py
@@ -1,8 +1,9 @@
 class Zodiac:
     """Find zodiac sign in birthdays.
     """
-    def __init__(key):
+    def zodiac(key):
         zodiac_list = {'March 21': 'Aries', 'April 20': 'Taurus', 'May 21': 'Gemini', 'June 21': 'Cancer', 'July 23': 'Leo', 'August 23': 'Virgo', 'September 23': 'Libra', 'October 23': 'Scorpio', 'November 22': 'Sagittarius', 'December 22': 'Capricorn', 'January 20': 'Aquarius', 'February 19': 'Pisces'}
+        
         zodiac = input("Please input your birthday to find your Zodiac sign.\n")
         value = zodiac_list.get(zodiac, "This is not an inputtable birthday.")
         print(value)
