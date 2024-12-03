1a4193a7b98c5fcc3c5d5874190215c2290bf580
dots <181677440+dots05@users.noreply.github.com>
2024-12-03 14:15:25 -0500

    Update zodiac.py

--- zodiac.py
+++ zodiac.py
@@ -2,8 +2,19 @@ class Zodiac:
     """Find zodiac sign in birthdays.
     """
     def __init__(key):
+        failure_count = 0
         zodiac_list = {'March 21': 'Aries', 'April 20': 'Taurus', 'May 21': 'Gemini', 'June 21': 'Cancer', 'July 23': 'Leo', 'August 23': 'Virgo', 'September 23': 'Libra', 'October 23': 'Scorpio', 'November 22': 'Sagittarius', 'December 22': 'Capricorn', 'January 20': 'Aquarius', 'February 19': 'Pisces'}
-        
-        zodiac = input("Please input your birthday to find your Zodiac sign.\n")
-        value = zodiac_list.get(zodiac, "This is not an inputtable birthday. Please try again.")
-        print(value)
+        while True:
+            zodiac = input("Please input your birthday to find your Zodiac sign.\n")
+            if zodiac in zodiac_list:
+                value = zodiac_list.get(zodiac)
+                print(value + "\n")
+                break
+            else:
+                failure_count += 1
+                if failure_count > 4:
+                    print("Fuck off")
+                    break
+                else:
+                    print("This is not an inputtable birthday. Please try again.\n")
+                    continue
