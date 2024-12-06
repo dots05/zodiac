c74c91fa7779e2fc447ae27dbb1028394f613d78
dots <181677440+dots05@users.noreply.github.com>
2024-12-06 12:45:03 -0500

    Create zodiac_calendar.py

---
+++ zodiac_calendar.py
@@ -0,0 +1,14 @@
+import datetime
+ 
+aries_start = datetime.date(2000, 3, 21)
+aries_end = datetime.date(2000, 4, 20)
+list = []
+delta = datetime.timedelta(days=1)
+ 
+# iterate over range of dates
+while (aries_start <= aries_end):
+    list.append(aries_start.strftime("%B %d"))
+    aries_start += delta
+
+tuple = tuple(list)
+zodiac_list = {tuple: 'Aries'}
