7023ae03d4852ba4e4652914c030d12adda23091
dots <181677440+dots05@users.noreply.github.com>
2024-12-22 21:46:09 -0500

    add comments

--- zodiac_calendar.py
+++ zodiac_calendar.py
@@ -1,15 +1,18 @@
+# add imports
 import datetime
- 
+
+#initialize variables
 start = datetime.date(1999, 3, 21)
 end = datetime.date(2000, 3, 20)
 date_list = []
 zodiac_list = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capricorn', 'Aquarius', 'Pisces']
-
 delta = datetime.timedelta(days=1)
+
 # iterate over range of dates
 while (start <= end):
     date_list.append(start.strftime("%B %d"))
     start += delta
-
+    
+#convert date_tuple to date_lust
 date_tuple = tuple(date_list)
 birthday_list = {date_tuple: zodiac_list}
